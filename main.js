d9d95d8d07a949c1b36c6a5eaa0d0b15fc1c3725
sukanya10991 <78801848+sukanya10991@users.noreply.github.com>
2021-09-21 18:56:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-function preload(){
+function preload() {
 
 }
-function setup(){
-    canvas= createCanvas(400, 400);
+
+function setup() {
+    canvas = createCanvas(400, 400);
     canvas.center();
-    
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wMNdEbw_x/model.json', modelLoaded);
+
 }
-function draw(){
-    
+
+function modelLoaded() {
+    console.log('Model Loaded!')
+}
+
+function draw() {
+    image(video, 0, 0, 400, 400);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("person").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
